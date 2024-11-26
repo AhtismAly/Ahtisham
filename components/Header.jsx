df4d72a42738b6e003282289497a500d7cb3373a
AhtismAly <engr.ahtismaly@gmail.com>
2024-11-26 14:25:50 +0000

    Ahtism

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,19 +3,19 @@ import Socials from "../components/Socials";
 
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full items-center px-16 xl-px-0 xl:h-[90px]">
+    <header className="absolute z-30 w-full items-center px-8 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-4">
           {/* Name styled with Athena font */}
           <Link href="/">
             <h1 className="text-white flex items-center">
               <span
-                className="text-6xl font-extrabold"
+                className="text-3xl font-extrabold"
                 style={{ fontFamily: "'Athena', sans-serif" }} // Apply Athena only to Ahtisham
               >
                 Ahtisham
               </span>
-              <span className="text-4xl font-normal ml-2">Ali Baig</span>
+              <span className="text-2xl font-normal ml-2">Ali Baig</span>
             </h1>
           </Link>
 
