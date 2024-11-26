e3330d6604abdc20ae3d99abf5e8a020f4c68c9f
AhtismAly <engr.ahtismaly@gmail.com>
2024-11-26 17:13:56 +0000

    Ahtism

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -29,8 +29,8 @@ const Work = () => {
               exit="hidden"
               className="mb-4 max-w-[400px] mx-auto lg:mx-0"
             >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              tempora eligendi, quisquam tempore sint reiciendis.
+              Dedicated to designing and constructing safe, 
+              sustainable, and efficient infrastructure solutions.
             </motion.p>
           </div>
 
