6c66d953c24d784735d53e431eff3379eba179bd
AhtismAly <your-email@example.com>
2025-10-10 12:38:12 +0200

    Updated website with portfolio content

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -42,13 +42,18 @@ const workSlides = {
           link: "https://1drv.ms/f/c/c140ce58107a3489/EmJ38L8JV_JEnQuoD8ltp04BSJ37zGpBUhw1DXLMdppmjg?e=5z5hVZ",
         },
         {
-          title: "The Effect of Dry Density on Shear Strength Parameters",
+          title: "IFC GUD Correction",
           path: "/thumb6.jpg",
+          link: "https://1drv.ms/f/c/c140ce58107a3489/EmrcZDQAqLxBgpejCEkIxSgBAz5Hk8BcHjylSWMl_CkhYg?e=ChymJn",
+        },
+        {
+          title: "The Effect of Dry Density on Shear Strength Parameters",
+          path: "/thumb7.jpg",
           link: "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4644744",
         },
         {
           title: "First Aid for Scouts",
-          path: "/thumb7.jpg",
+          path: "/thumb8.jpg",
           link: "https://drive.google.com/file/d/18bztQp2VaWik_PbxDQ7WX6_kcvRzB_fv/view?usp=drive_link",
         },
       ],
